33512960cb59edaa71eceb0a10a95cc17087363e
royalexchangegeneral <75024044+royalexchangegeneral@users.noreply.github.com>
2023-12-07 14:48:58 +0100

    first commit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,20 +5,8 @@ if [ ! -f "vendor/autoload.php" ]; then
 fi
 
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
+    echo "Creating env file for env "
     cp .env.example .env
-    case "$APP_ENV" in
-    "local")
-        echo "Copying .env.example ... "
-        cp .env.example .env
-    ;;
-    "prod")
-        echo "Copying .env.prod ... "
-        cp .env.prod .env
-    ;;
-    esac
-else
-    echo "env file exists."
 fi
 
 # php artisan migrate
